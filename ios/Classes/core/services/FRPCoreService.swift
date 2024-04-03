5d03650d8cdee70156a7d463f9965e37d4231582
Michel Melo <michelmelo@users.noreply.github.com>
2024-04-03 17:49:38 +0100

    Update FRPCoreService.swift

--- ios/Classes/core/services/FRPCoreService.swift
+++ ios/Classes/core/services/FRPCoreService.swift
@@ -47,7 +47,7 @@ class FRPCoreService: NSObject {
         
         // add listeners
         player.event.stateChange.addListener(self, FRPPlayerEventHandler.handleAudioPlayerStateChange)
-        player.event.receiveMetadata.addListener(self, FRPPlayerEventHandler.handleMetaDataChanges)
+        //player.event.receiveMetadata.addListener(self, FRPPlayerEventHandler.handleMetaDataChanges)
         
         audioSession.addObserver(self, forKeyPath: #keyPath(AVAudioSession.outputVolume), options: [.new, .initial], context: nil)
     }
